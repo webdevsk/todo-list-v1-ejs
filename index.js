cf877dd8de9bc232ab581cf00f8d07253265f795
webdevzsk <72684302+webdevsk@users.noreply.github.com>
2023-06-09 14:14:10 +0600

    Initialize mongodb connection and retrieve tasks

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require('express')
 const port = process.env.PORT || 3000
 const Date = require('./date')
-
-const homeTasks = ["Wake up", "Brush teeth"]
-const officeTasks = ["Setup a meeting", "Call secretarty"]
+const mongoose = require('mongoose')
+const {HomeTasks, OfficeTasks} = require('./home-tasks')
 
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: 'true'}))
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
+mongoose.connect('mongodb://127.0.0.1:27017/todoDB')
+
+async function getTasks(taskType){
+    try {
+        let data
+        if (taskType === 'HomeTasks') data = await HomeTasks.find()
+        if (taskType === 'OfficeTasks') data = await OfficeTasks.find()
+        // mongoose.disconnect()
+        return data
+    } catch (error) {
+        throw error
+    }
+}
+
+
+
+// const weekday = Date.getWeekDay()
+// const homeTasks = HomeTask.find()
+
+// console.log(homeTasks)
+
+
+
+app.get('/', async (req, res) => {
+    // const officeTasks = await OfficeTask.find()
+    const homeTasks = await getTasks('HomeTasks').catch(err => console.log(err))
     const weekday = Date.getWeekDay()
     const date = Date.getDate()
     const listType = 'homeTasks'
@@ -18,7 +42,8 @@ app.get('/', (req, res) => {
     res.render('todo', {weekday: weekday, date: date, todoList: homeTasks, listType: listType, listTitle: listTitle})
 })
 
-app.get('/office', (req, res)=>{
+app.get('/office', async (req, res)=>{
+    const officeTasks = await getTasks('OfficeTasks').catch(err => console.log(err))
     const weekday = Date.getWeekDay()
     const date = Date.getDate()
     const listType = 'officeTasks'

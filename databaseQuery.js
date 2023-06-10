383b61f460489716ddb3fd491652affbefe88e8d
webdevzsk <72684302+webdevsk@users.noreply.github.com>
2023-06-10 19:04:02 +0600

    Incorporated Dynamic category system and finalised Get method

--- databaseQuery.js
+++ databaseQuery.js
@@ -1,61 +1,118 @@
 const mongoose = require('mongoose')
 
+const categorySchema = new mongoose.Schema({
+    name: String,
+})
+
+const Category = mongoose.model('category', categorySchema)
+
 const taskSchema = new mongoose.Schema({
     label: {
         type: String,
         required: [1, "No label no task"]
     },
     completed: Boolean,
+    category: {
+        type: mongoose.Types.ObjectId,
+        ref: 'category'
+    }
 })
+const Task = mongoose.model('task', taskSchema)
 
 mongoose.models = {}
 
-const HomeTasks = mongoose.model('HomeTask', taskSchema)
-exports.HomeTasks = HomeTasks
+exports.task = Task
 
-const OfficeTasks = mongoose.model('OfficeTask', taskSchema)
-exports.OfficeTasks = OfficeTasks
-
-async function getTasks(taskType){
+// exports.category = Category
+getCategoryId('home').then(data => console.log(data))
+async function getCategoryId(categoryName){
     try {
-        let data
-        if (taskType === 'HomeTasks') data = await HomeTasks.find()
-        if (taskType === 'OfficeTasks') data = await OfficeTasks.find()
-        return data
-    } catch (err) {
-        console.log(err)
+        const ref = await Category.findOneAndUpdate({name: categoryName}, {}, {upsert:true})
+        return ref.id
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.getCategoryId = getCategoryId
+
+async function getTasks(categoryId){
+    try {
+        return await Task.find({category: categoryId}).populate('category')
+    } catch (error) {
+        console.log(error)
     }
 }
 
 exports.getTasks = getTasks
 
-async function postTasks(taskType, task){
+async function postTask(category, task){
     try {
-        let data
-        if (taskType === 'HomeTasks'){
-            await HomeTasks.insertMany([{label: task}])
-        }
-        if (taskType === 'OfficeTasks'){
-            await OfficeTasks.insertMany([{label: task}])
-        }
-    } catch (err) {
-        console.log(err)
+        await Task.insertMany([{category: category, label: task}])
+    } catch (error) {
+        console.log(error)
     }
 }
 
-exports.postTasks = postTasks
+exports.postTask = postTask
 
-async function deleteTask(taskType, taskId){
+async function deleteTask(taskId){
     try {
-        if (taskType === 'HomeTasks'){
-            await HomeTasks.deleteOne({_id: taskId})
-        }
-        if (taskType === 'OfficeTasks'){
-            await OfficeTasks.deleteOne({_id: taskId})
-        }
-    } catch (err) {
-        console.log(err)
+        await Task.deleteOne({_id: taskId})
+    } catch (error) {
+        console.log(error)
     }
 }
 
 exports.deleteTask = deleteTask
+
+async function deleteTasks(taskIdArr){
+    try {
+        await Task.deleteMany({_id: {$in: taskIdArr}})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.deleteTasks = deleteTasks
+
+async function updateTask(taskId, taskCompleted){
+    console.log(taskCompleted)
+    try {
+        const res = await Task.updateOne({_id: taskId}, {completed: taskCompleted})
+        console.log(res.modifiedCount)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.updateTask = updateTask
+
+// Run once to populate empty tasks
+// postprebuiltTasks('648463c23a0d1fadf66b8928')
+
+async function postprebuiltTasks(id){
+    try {
+        await Task.insertMany([
+            {
+                label: 'Welcome to Sam\'s TODOList', category: id
+            },
+            {
+                label: '<=== Click here if task is done', category: id
+            },
+            {
+                label: 'Click here to remove the task ===>', category: id
+            }
+        ])
+    } catch (error) {
+        console.log(error)
+    }
+}
+// Delete all prebuilt tasks
+
+// deleteAll()
+async function deleteAll(){
+    try{
+        await Task.deleteMany()
+    } catch (error){console.log(error)}
+}
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000
 const Date = require('./date')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const {getTasks, postTasks, deleteTask} = require('./databaseQuery')
+const {getCategoryId, getTasks, postTask, deleteTask, deleteTasks, updateTask} = require('./databaseQuery')
 
 const app = express()
 app.set('view engine', 'ejs')
@@ -19,49 +19,25 @@ app.use(cors(corsOptions))
 mongoose.connect('mongodb://127.0.0.1:27017/todoDB')
 
 app.get('/', async (req, res) => {
-    // const officeTasks = await OfficeTask.find()
-    const homeTasks = await getTasks('HomeTasks')
     const weekday = Date.getWeekDay()
     const date = Date.getDate()
-    const listType = 'homeTasks'
-    const listTitle = 'Home Tasks'
-    res.render('todo', {weekday: weekday, date: date, todoList: homeTasks, listType: listType, listTitle: listTitle})
-})
-
-app.get('/office', async (req, res)=>{
-    const officeTasks = await getTasks('OfficeTasks')
-    const weekday = Date.getWeekDay()
-    const date = Date.getDate()
-    const listType = 'officeTasks'
-    const listTitle = 'Office Tasks'
-    res.render('todo', {weekday: weekday, date: date, todoList: officeTasks, listType: listType, listTitle: listTitle})
+    const category = 'home'
+    const categoryId = await getCategoryId(category)
+    const homeTasks = await getTasks(categoryId)
+    res.render('todo', {weekday: weekday, date: date, todoList: homeTasks, category: category})
 })
 
 app.post('/', async (req, res) => {
-    const {task, listType} = req.body
+    const {task, category} = req.body
 
-    if (listType === "homeTasks"){
-        // homeTasks.push(task)
-        await postTasks('HomeTasks', task)
-        res.redirect('/')
-    } else if (listType === "officeTasks"){
-        // officeTasks.push(task)
-        await postTasks('OfficeTasks', task)
-        res.redirect('/office')
-    }
+    await postTask(category, task)
+    res.redirect('/')
 })
 
 app.post('/checkStatus', async (req, res) => {
-    const {taskId, listType} = req.body
-    if (listType === "homeTasks"){
-        // homeTasks.push(task)
-        await deleteTask('HomeTasks', taskId)
-        res.redirect('/')
-    } else if (listType === "officeTasks"){
-        // officeTasks.push(task)
-        await deleteTask('OfficeTasks', taskId)
-        res.redirect('/office')
-    }
+    const {taskId, completed} = req.body
+    await updateTask(taskId, completed && completed === 'on' ? true : false)
+    res.redirect('/')
 })
 
 app.listen(port, () => console.log('Server started on port ' + port))
